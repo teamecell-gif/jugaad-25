afd87bfebf7afa6348a30e5fee1b50737a285472
AryanSinghGangwar <aryansingh81104@gmail.com>
2026-02-03 17:13:32 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-import Sponser from "./components/Sponsor";
+// REMOVED: import Sponser from "./components/Sponsor"; 
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
 import Register from "./components/Register";
